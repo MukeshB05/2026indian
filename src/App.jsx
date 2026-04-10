68ec5e3bab80d782b3ae847c14fbc22c744bd6f0
Mukesh <mukesh2305b@gmail.com>
2026-04-10 19:32:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const JSON_URL = null;
 const STREAMS = [
   { name: "Star Sports 1 Tam", url: "https://dreamly-stream.pages.dev/SS1THD.html" },
   { name: "Star Sports 1 Eng", url: "https://dreamly-stream.pages.dev/SS1HDENG.html" },
-  { name: "Star Sports 1 Hin", url: "https://dreamly-stream.pages.dev/SSHD1HIN.html" },
+  { name: "Star Sports 1 Hin", url: "https://dreamly-stream.pages.dev/Hotstarhin.html" },
   { name: "Star Sports 1 Tel", url: "https://dreamly-stream.pages.dev/SS1TELHD.html" }
 ];
 
